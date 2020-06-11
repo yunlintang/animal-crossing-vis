5edaeebcbee43693935dc4bfc39460876a5ae866
yunlintang <yut085@ucsd.edu>
2020-06-11 01:22:08 -0700

    update

--- final.js
+++ final.js
@@ -17,6 +17,8 @@ function init(){
     }) */
     renderVillagersPie(villagers);
     renderVillagersColumn(villagers);
+    lineChart(switches);
+    areaChart(games);
 }
 
 function renderVillagersPie(data){
@@ -573,4 +575,215 @@ function drawCollectsTooltip(name,data){
     }
 
     return '<p style="font-size:13px;font-weight:bold">'+name+': '+total+' in total</p><table><tr>'+images+'</tr></table>';
+}
+
+
+function lineChart(switches){
+	
+	newData = extractData(switches);
+	//console.log(newData);
+	
+	var chart = Highcharts.chart('line',{
+        chart:{
+            type:"line"
+        },
+        title:{
+            text:"Interest Over Times of Searching: 'Switch'"
+		},
+
+		xAxis:{
+            type:'datetime',
+            title:{
+                text:"Date"
+            },
+            labels:{
+                align:'center',
+                format:'{value:%b/%d/%y}',
+			},
+            
+		},
+		yAxis: {
+            tickInterval:10,
+		},
+		legend:{
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign:'top',
+            x: -40,
+            y: 80,
+            floating: true,
+            borderWidth: 1,
+            backgroundColor:
+                Highcharts.defaultOptions.legend.backgroundColor || '#FFFFFF',
+            shadow: true,
+		},
+		tooltip: {
+			useHTML: true,
+			formatter: function() {
+				//console.log(this);
+				if((this.x >= Date.parse('2017-03-05'))&&(this.x <= Date.parse('2017-04-02'))){
+					var game1 = 'The Legend of Zelda: Breath of the Wild';
+					var game2 = 'Mario Kart 8 Deluxe'
+					var img1 = '<img src = "https://gamespot1.cbsistatic.com/uploads/scale_medium/1197/11970954/3181241-ig-lozbreathofthewildrelease-20170112.jpg" height="82" width="122"/>'
+					var img2 = '<img src = "https://cdn-cf.gamivo.com/image_cover.jpg?f=40001&n=7867014690560508.jpg&h=3fff81d693cb92d00132bf33cb8bc79d" height="82" width="122"/>'
+					var time = convertTime(this.x)
+					
+					return img1+img2+'<br/>'+game1+'<br/>'+game2+'<br/>'+'Interest over time: '+this.y+'<br/>'+time;
+				}else if((this.x >= Date.parse('2017-10-15'))&&(this.x <= Date.parse('2018-01-21'))){
+					var img1 = '<img src = "https://upload.wikimedia.org/wikipedia/en/8/8d/Super_Mario_Odyssey.jpg" height="82" width="122"/>'
+					var game = 'Super Mario Odyssey';
+					var time = convertTime(this.x)
+					return img1+'<br/>'+game+'<br/>'+'Interest over time: '+this.y+'<br/>'+time;
+				}else if((this.x >= Date.parse('2018-10-28'))&&(this.x <= Date.parse('2019-01-27'))){
+					var img1 = '<img src = "https://i.ytimg.com/vi/RWJU1bRu-OY/maxresdefault.jpg" height="82" width="122"/>'
+					var img2 = '<img src = "https://upload.wikimedia.org/wikipedia/en/6/6c/Super_Mario_Party.jpg" height="82" width="122"/>'
+					var game1 = "Pokémon: Let's Go, Pikachu! and Let's Go, Eevee!";
+					var game2 = "Super Mario Party";
+					var time = convertTime(this.x);
+					return img1+img2+'<br/>'+game1+'<br/>'+game2+'<br/>'+'Interest over time: '+this.y+'<br/>'+time;
+                }else if((this.x >= Date.parse('2019-08-18'))&&(this.x <= Date.parse('2019-10-06'))){
+                    var img = '<img src = "https://upload.wikimedia.org/wikipedia/en/3/38/Luigi%27s_Mansion_3.jpg" height="82" width="122"/>'
+					var game = "Luigi's Mansion 3"
+					var time = convertTime(this.x);
+					return img+'<br/>'+game+'<br/>'+'Interest over time: '+this.y+'<br/>'+time;
+                }else if((this.x >= Date.parse('2019-10-27'))&&(this.x <= Date.parse('2020-01-19'))){
+                    var img = '<img src = "https://pokemonletsgo.pokemon.com/assets/icons/share_icon-fb_en-us.jpg" height="82" width="122"/>'
+					var game = 'Pokémon Sword and Shield';
+					var time = convertTime(this.x);
+					return img+'<br/>'+game+'<br/>'+'Interest over time: '+this.y+'<br/>'+time;
+                }else if((this.x >= Date.parse('2020-03-08'))&&(this.x <= Date.parse('2020-05-31'))){
+                    var img = '<img src = "https://i2.wp.com/jsuchanticleer.com/wp-content/uploads/2019/10/animal-crossing.jpg?fit=354%2C198&ssl=1" height="82" width="122"/>'
+					var game = "Animal Crossing: New Horizons";
+					var time = convertTime(this.x);
+					return img+'<br/>'+game+'<br/>'+'Interest over time: '+this.y+'<br/>'+time;
+                }
+				else{
+					var time = convertTime(this.x);
+					return 'Interest over time: '+this.y+'<br/>'+time;
+				}
+
+			}
+		  },
+		series:[{
+			data: newData,
+			name: "Switch"
+        }],
+        credits:{
+            enabled: false,
+        }
+	});
+	
+}
+
+
+function areaChart(games){
+	var animal = [];
+	var pokemon = [];
+	var mario = [];
+	for(var i = 0;i < games.length;i++){
+        animal.push([games[i].Week, games[i]["animal crossing: (Worldwide)"]]);
+        pokemon.push([games[i].Week, games[i]["pokemon: (Worldwide)"]]);
+        mario.push([games[i].Week, games[i]["mario: (Worldwide)"]]);
+	}
+	var xAxis = getDate(games)
+	console.log(xAxis);
+	Highcharts.chart('container',{
+		chart: {
+			type: 'area'
+		},
+		title:{
+			text: "Interest Over Times of Seaching among Three Games"
+		},
+		xAxis:{
+			categories: xAxis,
+			label:{
+                align:'left'
+            },
+            tickmarkPlacement:'on',
+            title: {
+                text: 'Month'
+            },
+            lineWidth:1.5,
+			lineColor: '#000000',
+			labels:{
+                format:'{value:%b/%d/%y}',
+			},
+			plotLines: [{
+				value: 10.5,
+            	color: 'darkblue',
+				width: 2, 
+				zIndex: 5,
+				label:{
+					formatter: function () {
+						return 'Switch Released: 03/20/2020';
+					}
+				}
+			  }]
+		},
+		yAxis: {
+            tickInterval:10,
+		},
+		plotOptions: {
+			area: {
+				stacking: 'percent',
+				lineColor: '#ffffff',
+				lineWidth: 1,
+				marker: {
+					lineWidth: 1,
+					lineColor: '#ffffff'
+				},
+			},
+			series: {
+				marker: {
+					enabled: false
+				}
+			}
+		},
+		series:[{
+				name:'Animal Crossing',
+				data: animal,
+				color: "rgba(245, 227, 125, 0.8)"
+			},{
+				name:'Pokemon',
+				data: pokemon,
+				color: "rgba(75,185,130,0.8)"
+				
+			},{
+				name: 'Mario',
+				data: mario,
+				color: "rgba(97,181,223,0.8)"
+            }],
+        credits:{
+            enabled: false
+        }
+	})
+}
+function getDate(games){
+    let date = [];
+    for(var i = 0;i < games.length;i++){
+		date.push(convertTime(Date.parse(games[i].Week)));
+		 
+    }
+    return date;
+}
+function convertTime(unixTime){
+	
+	var date = new Date(unixTime)
+	var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  	var year = date.getFullYear();
+  	var month = months[date.getMonth()];
+	var day = date.getDate();
+	var time = month + ' ' + day + ' ' + year;  
+	return time;
+}
+function extractData(data){
+    let searchSwitch = [];
+    for(var i = 0;i < data.length;i++){
+        //date single
+		let dateS = Date.parse(data[i].Week);
+		let search = data[i]['switch: (Worldwide)'];
+        
+        searchSwitch.push([dateS,search]);
+    }
+    return searchSwitch;
 }
